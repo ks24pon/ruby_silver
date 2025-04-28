806eb9dd63c62ffec4716c231f7775b2fa0412f4
ks24pon <pom123re0624@gmail.com>
2025-04-28 10:57:37 +0900

    04/28

---
+++ mock/sample28.rb
@@ -0,0 +1,10 @@
+# aに"Rub"代入
+a = "Ruby"
+# bに"Rails"代入
+b = "Rails"
+# bにaの内容を代入
+b = a
+# aを大文字に変換するが非破壊メソッドなのでaは変わらない
+a.upcase
+# b出力
+print b
---
+++ mock/sample29.rb
@@ -0,0 +1,2 @@
+# !という文字が5番目以降で最初に出てくる位置を探す
+p "find!find!find!find!find!find".index("!", 5)

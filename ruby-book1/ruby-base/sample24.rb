33ddace76dfffc9b8378fd9a680fd45dda00c9fe
ks24pon <pom123re0624@gmail.com>
2025-04-30 13:41:04 +0900

    4/30

---
+++ ruby-book1/ruby-base/sample24.rb
@@ -0,0 +1,10 @@
+# 配列memberに整数IDと名前を格納
+member = [10, "Tanaka"]
+print "ID:%2d Name:%s" % member
+
+# %2d：幅2文字で整数を右寄せ表示
+# %s：文字列をそのまま挿入
+# %の右側にある配列の各要素が左側のフォーマット順番に埋め込まれる
+# 出力
+# ID:10 Name:Tanaka
+

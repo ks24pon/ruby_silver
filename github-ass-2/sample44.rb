fd5f165894343d41d771296ca6dd91f006fe0d40
ks24pon <pom123re0624@gmail.com>
2025-05-23 15:20:31 +0900

    q-44

---
+++ github-ass-2/sample44.rb
@@ -0,0 +1,8 @@
+capital = {:Sweden => "Stockholm", :Norway => "Oslo", :Finland => "Helsinki"}
+p capital
+
+capital1 = {Sweden: "Stockholm", Norway: "Oslo", Finland: "Helsinki"}
+p capital1
+
+capital3 = Hash[:Sweden, "Stockholm", :Norway, "Oslo", :Finland, "Helsinki"]
+p capital3

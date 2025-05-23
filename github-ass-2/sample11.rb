b5ef536204325825309e0960d526b499fefbdcd7
ks24pon <pom123re0624@gmail.com>
2025-05-23 10:42:12 +0900

    q-11

---
+++ github-ass-2/sample11.rb
@@ -0,0 +1,20 @@
+h = {1=>2, 3=>4}
+h.clear
+p h
+
+ary = [1,2]
+ary.clear
+p ary
+
+
+a = [1, 2, 3]
+b = [4, 5]
+
+a.replace(b)
+p a
+
+[].empty? # => true
+[1, 2, 3].empty? # => false
+"".empty? # => true
+"abc".empty? # => false
+{}.empty? # => true

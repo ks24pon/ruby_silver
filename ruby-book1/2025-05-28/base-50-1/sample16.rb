b5207125096d06536fd00a41313a09eb5efae7a2
ks24pon <pom123re0624@gmail.com>
2025-05-30 10:47:47 +0900

    Q16

---
+++ ruby-book1/2025-05-28/base-50-1/sample16.rb
@@ -0,0 +1,23 @@
+# to_s
+p 123.to_s
+p [1, 2, 3].to_s
+
+x = 100
+puts "Value is " + x.to_s
+
+# join
+puts ["a", "b", "c"].join
+puts ["a", "b", "c"].join(",")
+
+# concat
+a = [1, 2]
+a.concat([3, 4])
+p a
+
+s = "Hello"
+s.concat(" World")
+p s
+
+# to_a
+p (1..3).to_a
+
